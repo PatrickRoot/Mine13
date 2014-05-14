7679e20bb83ec453e32695f3b568a8996ee810b2
sixlab <nianqinianyi@163.com>
2014-05-14 22:09:14 +0800

    又是好久没更新

--- Java环境变量.bat
+++ Java环境变量.bat
@@ -1,15 +1,15 @@
 @echo off
 echo %JAVA_HOME%
-echo %classPath%
+echo %CLASSPATH%
 echo %PATH%
 set/p jdkhome=请输入JDK目录并按回车：
 echo 输入为：%jdkhome%
 pause
 echo === 新创建环境变量 JAVA_HOME=%jdkhome%
-echo === 新创建环境变量 classPath=.;%%JAVA_HOME%%\lib\tools.jar;%%JAVA_HOME%%\lib\dt.jar
+echo === 新创建环境变量 CLASSPATH=.;%%JAVA_HOME%%\lib\tools.jar;%%JAVA_HOME%%\lib\dt.jar;%%JAVA_HOME%%\lib\
 echo === 新创建环境变量 PATH=%%JAVA_HOME%%\bin
 pause
 setx "JAVA_HOME" "%jdkhome%" -M
-setx "classPath" ".;%%JAVA_HOME%%\lib\dt.jar;%%JAVA_HOME%%\lib\tools.jar" -M
+setx "CLASSPATH" ".;%%JAVA_HOME%%\lib\dt.jar;%%JAVA_HOME%%\lib\tools.jar;%%JAVA_HOME%%\lib\" -M
 setx "PATH" "%path%;%%JAVA_HOME%%\bin" -M
 pause
---
+++ java/algorithm40/src/org/sixlab/algorithm40/CommonNumber.java
@@ -0,0 +1,41 @@
+package org.sixlab.algorithm40;
+
+public class CommonNumber {
+	public static void main(String[] args) {
+		System.out.println(leastCommonMultiple(70, 100));
+		System.out.println(greatestCommonDivisor(70, 100));
+	}
+	
+	private static int greatestCommonDivisor(int x, int y) {
+		int min = Math.min(x, y);
+		for (int i = min; i > 1; i--) {
+			if (x % i == 0 && y % i == 0) {
+				return i;
+			}
+		}
+		return 1;
+	}
+	
+	private static int leastCommonMultiple(int x, int y) {
+		int max = Math.max(x, y);
+		
+		int result = 1;
+		for (int i = 2; i < max; i++) {
+			if (x % i == 0 && y % i == 0) {
+				result *= i;
+				x /= i;
+				y /= i;
+				i--;
+			} else if (x % i == 0) {
+				result *= i;
+				x /= i;
+				i--;
+			} else if (y % i == 0) {
+				result *= i;
+				y /= i;
+				i--;
+			}
+		}
+		return result;
+	}
+}
