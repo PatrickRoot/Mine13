74596a5844a78ca8167da5abc2a7ed82ed34275e
六楼的雨 <nianqinianyi@163.com>
2016-03-02 13:34:03 +0800

    focus 油猴

---
+++ userScripts/focus.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Focus
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://www.2345.com/*
+// @grant        none
+// @require      http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js
+// ==/UserScript==
+/* jshint -W097 */
+
+// Your code here...
+
+console.log($("#content").hide());
+console.log($("#content").hide());
