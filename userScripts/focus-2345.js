38e9aee3fa627e9451753b864538666fbeb573c4
六楼的雨 <nianqinianyi@163.com>
2016-03-02 19:47:36 +0800

    jquery

--- userScripts/focus-2345.js
+++ userScripts/focus-2345.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Focus
 // @namespace    http://sixlab.cn/
-// @version      0.0.3
+// @version      0.0.5
 // @description  去掉网页上影响关注的内容!
 // @author       loki/六楼的雨
 // @match        http://www.2345.com/*
 // @grant        none
-// @require      http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js
+// @require      https://raw.githubusercontent.com/nianqinianyi/sixlab/master/userScripts/jquery.min.js
 // ==/UserScript==
 /* jshint -W097 */
 
@@ -15,14 +15,14 @@
 var className = "sixlab-focus";
 
 //2345
-$(".hotw_line1").addClass(className).hide();
-$("#content").addClass(className).hide();
-$("#map_game").addClass(className).hide();
-$("#map_buy").addClass(className).hide();
-$("#sidenav").addClass(className).hide();
-$("#map_video").addClass(className).hide();
-$("#map_these").addClass(className).hide();
-$("#map_life").addClass(className).hide();
-$("#J_category").addClass(className).hide();
-$("#footer").addClass(className).hide();
-$(".top_news").addClass(className).hide();
+$j(".hotw_line1").addClass(className).hide();
+$j("#content").addClass(className).hide();
+$j("#map_game").addClass(className).hide();
+$j("#map_buy").addClass(className).hide();
+$j("#sidenav").addClass(className).hide();
+$j("#map_video").addClass(className).hide();
+$j("#map_these").addClass(className).hide();
+$j("#map_life").addClass(className).hide();
+$j("#J_category").addClass(className).hide();
+$j("#footer").addClass(className).hide();
+$j(".top_news").addClass(className).hide();
