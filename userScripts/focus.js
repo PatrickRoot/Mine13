c70e329d9d4c23edd7f909adbc136df962136b85
六楼的雨 <nianqinianyi@163.com>
2016-03-02 13:40:32 +0800

    更新 focus.js

--- userScripts/focus.js
+++ userScripts/focus.js
@@ -12,5 +12,20 @@
 
 // Your code here...
 
-console.log($("#content").hide());
-console.log($("#content").hide());
+var className = "sixlab-focus";
+
+//2345
+var host = window.location.host;
+if ('www.2345.com' === host) {
+    $(".hotw_line1").addClass(html).hide();
+    $("#content").addClass(html).hide();
+    $("#map_game").addClass(html).hide();
+    $("#map_buy").addClass(html).hide();
+    $("#sidenav").addClass(html).hide();
+    $("#map_video").addClass(html).hide();
+    $("#map_these").addClass(html).hide();
+    $("#map_life").addClass(html).hide();
+    $("#J_category").addClass(html).hide();
+    $("#footer").addClass(html).hide();
+    $(".top_news").addClass(html).hide();
+}
