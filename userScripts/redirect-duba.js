66ed1951a92fefc60a5256837960b1402499455e
六楼的雨 <nianqinianyi@163.com>
2016-03-28 10:45:15 +0800

    毒霸转向

---
+++ userScripts/redirect-duba.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Redirect Duba
+// @namespace    http://sixlab.cn/
+// @version      0.0.8
+// @description  去掉网页上影响关注的内容!
+// @author       loki/六楼的雨
+// @include      *://www.duba.com/*
+// @grant        none
+// @require      https://raw.githubusercontent.com/nianqinianyi/sixlab/master/userScripts/jquery.min.js
+// ==/UserScript==
+/* jshint -W097 */
+
+// Your code here...
++function(){
+	location.href = 'http://www.2345.com/?k1069702'
+}();
